74c4f9c7140e7bfa09ff18fb55f0c0107140bae8
Léon Logli <leonlogli@gmail.com>
2020-10-26 06:10:36 +0100

    Add initial load support

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -8,21 +8,32 @@ import { useRef, useEffect } from "react";
  * @param {Function} options.onLoadMore The callback function a function which must be called after
  *    reaching the bottom. It must trigger some sort of action which fetches the next data
  * @param {boolean} options.hasMore The observer will disconnect when there are no more data to load.
+ * * @param {boolean} [options.initialLoad=true] Whether to call `onLoadMore` function on initial render.
  * @param {boolean} [options.init=true] Whether the component should load the first set of items
  * @param {number} [options.threshold=250] The distance in px before the end of the items that will trigger a call to `onLoadMore`
  */
-function useInfiniteScroll({ hasMore, onLoadMore, threshold = 250 }) {
+function useInfiniteScroll({
+  hasMore,
+  onLoadMore,
+  threshold = 250,
+  initialLoad = true,
+}) {
   const scrollerRef = useRef();
   const loaderRef = useRef();
+  const runCount = useRef(0);
 
   const onLoadMoreRef = useRef();
   onLoadMoreRef.current = onLoadMore;
 
   useEffect(() => {
+    runCount.current++;
+  });
+
+  useEffect(() => {
     const loader = loaderRef.current;
     const scroller = scrollerRef?.current;
 
-    if (!loader || !hasMore) return;
+    if (!loader || !hasMore || !onLoadMoreRef.current) return;
 
     const options = {
       root: scroller,
@@ -46,7 +57,9 @@ function useInfiniteScroll({ hasMore, onLoadMore, threshold = 250 }) {
           (!previousY || boundingClientRect.y < previousY);
 
         if (canLoadMore) {
-          if (onLoadMoreRef.current) {
+          if (initialLoad) {
+            onLoadMoreRef.current();
+          } else if (runCount.current > 2) {
             onLoadMoreRef.current();
           }
         }
@@ -60,7 +73,7 @@ function useInfiniteScroll({ hasMore, onLoadMore, threshold = 250 }) {
     observer.observe(loader);
 
     return () => observer.disconnect();
-  }, [hasMore, threshold]);
+  }, [hasMore, initialLoad, threshold]);
 
   return { loaderRef, scrollerRef };
 }
