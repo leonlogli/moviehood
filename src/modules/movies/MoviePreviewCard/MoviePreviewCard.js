6fb321cfcc411cb991909a0b4fb8fdbd19ec70dc
Léon Logli <leonlogli@gmail.com>
2020-10-27 08:56:23 +0100

    Improve MoviePreviewCard

--- src/modules/movies/MoviePreviewCard/MoviePreviewCard.js
+++ src/modules/movies/MoviePreviewCard/MoviePreviewCard.js
@@ -8,8 +8,14 @@ import { FavoriteIcon } from "../../favorites";
 import "./MoviePreviewCard.scss";
 
 const MoviePreviewCard = ({ movie, className = "" }) => {
-  const { id, title, coverImage, vote_average, vote_count } = movie;
+  const { id, title, coverImage, vote_average, release_date } = movie;
   const history = useHistory();
+
+  const date = new Date(release_date).toLocaleString(undefined, {
+    month: "short",
+    year: "numeric",
+    day: "numeric",
+  });
 
   const handleClick = () => {
     history.push(`/movies/${id}`);
@@ -20,9 +26,10 @@ const MoviePreviewCard = ({ movie, className = "" }) => {
       <img src={coverImage || noImg} alt="movie" />
       <div className="textBox">
         <h2>{title}</h2>
-        <span>
-          <FontAwesomeIcon icon={faStar} />
-          {vote_average + "/10 (" + vote_count + ")"}
+        <span className="subTitle">
+          {date}
+          <FontAwesomeIcon className="star" icon={faStar} />
+          {vote_average / 2}
         </span>
       </div>
       <FavoriteIcon movie={movie} />
